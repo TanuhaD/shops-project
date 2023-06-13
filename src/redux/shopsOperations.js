3b6b310ae1057fdf7d73713523b02885d1cc1630
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-06-13 22:24:15 +0300

    add new baseUrl

--- src/redux/shopsOperations.js
+++ src/redux/shopsOperations.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-axios.defaults.baseURL = "https://647aebf8d2e5b6101db0a5d3.mockapi.io/";
+// axios.defaults.baseURL = "https://647aebf8d2e5b6101db0a5d3.mockapi.io/";
+axios.defaults.baseURL = "https://nodejs-homework-demchenko.onrender.com/api/";
 
 export const getShops = createAsyncThunk("shops/get", async (_, thunkAPI) => {
   try {
